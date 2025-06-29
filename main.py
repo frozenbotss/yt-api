d9287055a702865569559835010ad640e8a53319
frozenbotss <prachisahlot@gmail.com>
2025-06-29 19:22:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import yt_dlp
+import os
 
 app = Flask(__name__)
 
@@ -113,6 +114,7 @@ def extract_info():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use $PORT if set, otherwise default to 5000
+    app.run(host='0.0.0.0', port=port)
 
 
